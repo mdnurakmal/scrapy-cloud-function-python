2691ea53a77d6ff1d44eed3609b8dfa36e445577
mdnurakmal <mdnurakmal@gmail.com>
2021-10-03 22:00:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ from scrapy.utils.project import get_project_settings
 def my_cloud_function(event, context):
     def script(queue):
         try:
-        settings = get_project_settings()
-        settings.setdict({
-        'LOG_LEVEL': 'ERROR',
-        'LOG_ENABLED': True,
-        })
-        process = CrawlerProcess(settings)
-        process.crawl(AfajofSpider)
-        process.start()
-        queue.put(None)
+            settings = get_project_settings()
+            settings.setdict({
+            'LOG_LEVEL': 'ERROR',
+            'LOG_ENABLED': True,
+            })
+            process = CrawlerProcess(settings)
+            process.crawl(AfajofSpider)
+            process.start()
+            queue.put(None)
         except Exception as e:
-        queue.put(e)
+            queue.put(e)
+            
     queue = Queue()
     # wrap the spider in a child process
 
