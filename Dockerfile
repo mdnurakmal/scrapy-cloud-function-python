624e3d7611fe95892be8e3304d662f89baba69f1
mdnurakmal <mdnurakmal@gmail.com>
2021-10-03 21:51:53 +0800

    update

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# Use the official Python image.
-FROM python:3.7
-
-# Install production dependencies.
-RUN pip install Scrapy
-RUN pip install scrapy-xlsx
-RUN chmod +x init.sh
-
-CMD scrapy crawl afajof
--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-359.0.0-linux-x86_64.tar.gz
-
-./google-cloud-sdk/install.sh
-
-./google-cloud-sdk/bin/gcloud init
