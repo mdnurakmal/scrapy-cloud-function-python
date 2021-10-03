2bb3a5d0ff2ca85b0b136491509e300c3feca87d
mdnurakmal <mdnurakmal@gmail.com>
2021-10-03 23:37:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from conferenceCalendar.spiders.afajofspiders import AfajofSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from flask import escape
+from datetime import datetime
+
 import subprocess
 import os
 def hello_http(request):
 
 
-    process = CrawlerProcess()
-    process.crawl(AfajofSpider)
-    process.start()
+    # process = CrawlerProcess()
+    # process.crawl(AfajofSpider)
+    # process.start()
 
+    now = datetime.now()
+    e = 'touch ' + shlex.quote(now)
+    subprocess.run(e, stdout=subprocess.PIPE, shell=True)
     c = 'gsutil cp $HOME/scrapy-cloud-function-python/afajof_calendar.xlsx gs://afajof_calendar'
 
     subprocess.run(c, stdout=subprocess.PIPE, shell=True)
