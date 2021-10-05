4e830649cff31f24e3ead7953941ca0aada14e7f
mdnurakmal <mdnurakmal@gmail.com>
2021-10-05 09:48:03 +0800

    Update upload.py

--- conferenceCalendar/upload.py
+++ conferenceCalendar/upload.py
@@ -4,11 +4,11 @@ import os
 
 def upload(myfile,destination):
     # Make an authenticated API request
+    print("filename : " + myfile)
+    print("destination : " + destination)
 
     storage_client = storage.Client()
     bucket = storage_client.bucket("afajof_calendar")
 
     blob = bucket.blob(destination)
     blob.upload_from_filename(myfile)
-    print("filename : " + myfile)
-    print("destination : " + destination)
