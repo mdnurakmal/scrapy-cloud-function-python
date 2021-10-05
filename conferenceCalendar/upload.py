f93beef28dc92f359d1fdd1e12ae3ff1a5bcf6d7
mdnurakmal <mdnurakmal@gmail.com>
2021-10-05 09:57:56 +0800

    Update upload.py

--- conferenceCalendar/upload.py
+++ conferenceCalendar/upload.py
@@ -1,14 +1,19 @@
 from google.cloud import storage
 from oauth2client.service_account import ServiceAccountCredentials
 import os
-
+    from os import path
 def upload(myfile,destination):
     # Make an authenticated API request
     print("filename : " + myfile)
     print("destination : " + destination)
 
-    storage_client = storage.Client()
-    bucket = storage_client.bucket("afajof_calendar")
+    root = path.dirname(path.abspath(__file__))
+    children = os.listdir(root)
+    files = [c for c in children if path.isfile(path.join(root, c))]
+    return 'Files: {}'.format(files)
 
-    blob = bucket.blob(destination)
-    blob.upload_from_filename(myfile)
+    # storage_client = storage.Client()
+    # bucket = storage_client.bucket("afajof_calendar")
+
+    # blob = bucket.blob(destination)
+    # blob.upload_from_filename(myfile)
