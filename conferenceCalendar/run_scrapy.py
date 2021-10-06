c3ab133a0728a256f2f3c018a8c9c435708eebf1
mdnurakmal <mdnurakmal@gmail.com>
2021-10-07 00:16:21 +0800

    update

---
+++ conferenceCalendar/run_scrapy.py
@@ -0,0 +1,35 @@
+
+
+def script(queue,return_dict):
+    try:
+
+        settings = get_project_settings()
+
+        process = CrawlerProcess(settings)
+        process.crawl(AfajofSpider)
+        process.start()
+
+        
+        
+        now = datetime.now().strftime("%m%d%Y_%H%M%S")
+        home = os.environ['HOME']
+        cwd = os.getcwd()
+        arr = os.listdir('.')
+        return_dict["print"]=arr
+        #subprocess.call(["mv", "/tmp/afajof_calendar.xlsx","/tmp/temp-"+now+".xlsx"])
+
+        return_dict["filename"]="temp-"+now+".xlsx"
+        #return_dict["temp"]=home+"/scrapy-cloud-function-python/afajof_calendar.xlsx"
+        
+        #os.system("gsutil cp $HOME/scrapy-cloud-function-python/{filename} gs://afajof_calendar/{filename}".format(filename="temp-"+now+".xlsx"))
+        
+        #upload("/tmp/temp-"+now+".xlsx"],"temp-"+now+".xlsx")
+
+        arr = os.listdir(os.path.abspath('/conferenceCalendar'))
+        return_dict["print1"]=arr
+
+
+        queue.put(None)
+    except Exception as e:
+        queue.put(e)
+
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from conferenceCalendar.spiders.afajofspiders import AfajofSpider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from conferenceCalendar.upload import upload
+from conferenceCalendar.run_scrapy import script
 from datetime import datetime
+
 import shlex 
 import subprocess
 import os
@@ -11,39 +13,6 @@ from multiprocessing import Process, Queue , Manager
 
 
 def hello_http(request):
-    def script(queue,return_dict):
-        try:
-
-            settings = get_project_settings()
-
-            process = CrawlerProcess(settings)
-            process.crawl(AfajofSpider)
-            process.start()
-
-           
-         
-            now = datetime.now().strftime("%m%d%Y_%H%M%S")
-            home = os.environ['HOME']
-            cwd = os.getcwd()
-            arr = os.listdir('.')
-            return_dict["print"]=arr
-            #subprocess.call(["mv", "/tmp/afajof_calendar.xlsx","/tmp/temp-"+now+".xlsx"])
-
-            return_dict["filename"]="temp-"+now+".xlsx"
-            #return_dict["temp"]=home+"/scrapy-cloud-function-python/afajof_calendar.xlsx"
-         
-            #os.system("gsutil cp $HOME/scrapy-cloud-function-python/{filename} gs://afajof_calendar/{filename}".format(filename="temp-"+now+".xlsx"))
-            
-            #upload("/tmp/temp-"+now+".xlsx"],"temp-"+now+".xlsx")
-
-            arr = os.listdir(os.path.abspath('/conferenceCalendar'))
-            return_dict["print1"]=arr
-
-
-            queue.put(None)
-        except Exception as e:
-            queue.put(e)
-
     queue = Queue()
     # wrap the spider in a child process
     manager = Manager()
